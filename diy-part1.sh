5671feb86c4e31249f1ae17a8bd660de673d21c9
会飞的小情书 <71999976+Look-UI@users.noreply.github.com>
2024-08-25 01:22:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,6 +22,12 @@
 ## 通用版luci适合18.06与19.07
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a 
 
 
 ## 解除系统限制
